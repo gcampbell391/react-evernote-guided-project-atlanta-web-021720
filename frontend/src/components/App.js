358b1b5fb79623f2bbef71bc7cb409cd79fbd8ee
gcampbell391 <gcampbell391@gmail.com>
2020-07-01 10:45:14 -0400

    first commit

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,23 @@ import Header from './Header';
 import NoteContainer from './NoteContainer';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      allNotes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/notes')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ allNotes: data })
+      })
+  }
+
   render() {
+    console.log(this.state.allNotes)
     return (
       <div className="app">
         <Header />
